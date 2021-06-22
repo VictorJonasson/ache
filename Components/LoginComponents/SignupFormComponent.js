2b57f9f8d6b400ee0c0b36aa662b412f35c6f86c
VictorJ <jonassonvictor@gmail.com>
2021-06-22 12:02:58 +0200

    New secure password input in signupform

--- Components/LoginComponents/SignupFormComponent.js
+++ Components/LoginComponents/SignupFormComponent.js
@@ -1,51 +1,93 @@
-import React from 'react';
-import {Image, StyleSheet} from 'react-native';
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
 import {
     Button,
     Input,
     Layout,
     Text,
-    Icon
-} from '@ui-kitten/components';
-import LogoProviderComponent from "../../Components/LogoProviderComponent";
-import {TouchableOpacity} from 'react-native-gesture-handler';
-import { useNavigation } from '@react-navigation/native';
-
+    Icon,
+} from "@ui-kitten/components";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableWithoutFeedback } from "@ui-kitten/components/devsupport";
 
 export const FacebookIcon = (props) => (
-    <Icon style={{ width: 22, height: 22 }} name='key' pack="feather"/>
+    <Icon style={{
+        width: 22,
+        height: 22,
+    }} name="key" pack="feather" />
 );
 export const UserIcon = (props) => (
-    <Icon style={{ width: 22, height: 22 }} name='user' pack="feather"/>
+    <Icon style={{
+        width: 22,
+        height: 22,
+    }} name="user" pack="feather" />
+);
+const AlertIcon = (props) => (
+    <Icon {...props} style={{
+        width: 22,
+        height: 22,
+    }} name="alert-circle-outline" />
 );
 
-
 const SignupFormComponent = () => {
+
+    const [value, setValue] = React.useState("");
+    const [secureTextEntry, setSecureTextEntry] = React.useState(true);
     const navigation = useNavigation();
-    const [password, setPassword] = React.useState('');
-    const [email, setEmail] = React.useState('');
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    const toggleSecureEntry = () => {
+        setSecureTextEntry(!secureTextEntry);
+    };
+
+    const renderIcon = (props) => (
+        <TouchableWithoutFeedback onPress={toggleSecureEntry}>
+            <Icon {...props} name={secureTextEntry ? "eye-off" : "eye"} />
+        </TouchableWithoutFeedback>
+    );
+
+    const renderCaption = () => {
+        return (
+            <View style={styles.captionContainer}>
+                {AlertIcon(styles.captionIcon)}
+                <Text style={styles.captionText}>Måste bestå av minst 8 tecken</Text>
+            </View>
+        );
+    };
 
     return (
-        <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Layout style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
             <Input
                 accessoryLeft={UserIcon}
                 style={styles.inputEmail}
                 value={email}
-                label='Email'
+                label="Email"
                 onChangeText={(nextValue) => setEmail(nextValue)}
             />
-
             <Input
-                accessoryLeft={FacebookIcon}
-                style={styles.inputPassword}
                 value={password}
-                label='Password'
-                secureTextEntry={true}
+                label="Password"
+                style={styles.inputPassword}
+                caption={renderCaption}
+                accessoryLeft={FacebookIcon}
+                accessoryRight={renderIcon}
+                secureTextEntry={secureTextEntry}
                 onChangeText={(nextValue) => setPassword(nextValue)}
             />
 
-            <Layout style={{flex: 0, width: '45%', justifyContent: 'center', alignItems: 'center'}}>
-                <Button size={'small'} style={styles.loginButton}
+            <Layout style={{
+                flex: 0,
+                width: "45%",
+                justifyContent: "center",
+                alignItems: "center",
+            }}>
+                <Button size={"small"} style={styles.loginButton}
                         onPress={console.log("Registrera")}>
                     Registrera
                 </Button>
@@ -57,19 +99,39 @@ const SignupFormComponent = () => {
 
 const styles = StyleSheet.create({
     inputEmail: {
-        width: '80%',
-        marginTop: '30%',
+        width: "80%",
+        marginTop: "30%",
+
     },
     signUp: {
-        marginTop: '20%',
+        marginTop: "20%",
+
     },
     inputPassword: {
-        width: '80%',
+        width: "80%",
         marginTop: 20,
+        color: "red",
     },
     loginButton: {
-        marginTop: '20%',
-    }
+        marginTop: "20%",
+        color: "#8F9BB3",
+    },
+    captionContainer: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    captionIcon: {
+        width: 10,
+        height: 10,
+        marginRight: 5,
+    },
+    captionText: {
+        fontSize: 12,
+        fontWeight: "400",
+        fontFamily: "opensans-regular",
+        color: "#8F9BB3",
+    },
 });
 
 export default SignupFormComponent;
